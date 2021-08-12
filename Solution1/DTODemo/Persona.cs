8629d601987908886ae8b52f2824bbe5fa3effde
Martin Cruz M <codemcm@hotmail.com>
2021-08-12 13:21:38 -0600

    database persona

---
+++ Solution1/DTODemo/Persona.cs
@@ -0,0 +1,18 @@
+﻿namespace DTODemo
+{
+
+    /*
+        CREATE DATABASE gente;
+        USE gente
+        CREATE TABLE Persona(
+        PersonaId int IDENTITY(1,1),  
+        Nombre varchar (20));
+
+     */
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int PersonaId { get; set; }
+
+    }
+}
