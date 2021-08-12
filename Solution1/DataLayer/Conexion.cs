5ba3e3abad89a1954f99460a07fa75cf1ae3083f
Martin Cruz M <codemcm@hotmail.com>
2021-08-12 13:32:56 -0600

    client for bd

---
+++ Solution1/DataLayer/Conexion.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    public class Conexion
+    {
+        string cadenaConexion = "Server=DESKTOP-5CIT38A;DataBase= gente;Integrated Security=true";
+        SqlConnection con;
+        public Conexion()
+        {
+            con = new SqlConnection(cadenaConexion);
+        }
+
+        public bool EsAbierta()
+        {
+            return con.State.Equals("Open");
+        }
+
+        public int Insertar(string query)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(query, con)
+            {
+                CommandType = CommandType.Text
+            };
+            int rowAffected = cmd.ExecuteNonQuery();
+            con.Close();
+            return rowAffected;
+        }
+    }
+}
